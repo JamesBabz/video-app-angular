ac2336e3a15d5bd726e08154e5fe07bc128acfcb
JamesBabz <web.fuhlendorff@gmail.com>
2017-09-25 23:27:05 +0200

    init
    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  firstValue = 0;
+  secondValue = 0;
+  calcExp = '+';
+
+
+  calculate() {
+    switch (this.calcExp) {
+      case '+':
+        return this.firstValue * 1 + this.secondValue * 1;
+      case '-':
+        return this.firstValue * 1 - this.secondValue * 1;
+      case '*':
+        return this.firstValue * 1 * this.secondValue * 1;
+      case '/':
+        return this.firstValue * 1 / this.secondValue * 1;
+      default:
+        return this.firstValue * 1 + this.secondValue * 1;
+    }
+
+  }
+}
+
+
